943cd3e6e83bf06d3161db0a9fa63e1a6bd584a0
naumaan-kalyani08 <naumaan.kalyani02@gmail.com>
2024-08-31 23:39:21 +0530

    new updates

--- src/components/MarqueeSlider.jsx
+++ src/components/MarqueeSlider.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import Marquee from "react-fast-marquee";
 
-const MarqueeComponent = ({ data, speed, direction , playStatus }) => {
+const MarqueeComponent = ({ data, speed, direction, playStatus }) => {
+    // Duplicate the data to ensure an infinite loop effect
+    const extendedData = [...data, ...data];
+
     return (
         <Marquee
             pauseOnHover={true}
@@ -10,7 +13,7 @@ const MarqueeComponent = ({ data, speed, direction , playStatus }) => {
             speed={speed}
             className="box-wrapper"
         >
-            {data.map((item) => (
+            {extendedData.map((item) => (
                 <div className="partner-logo-slider-wrapper" id={item.id} key={item.id}>
                     <img src={item.logo} alt="" />
                 </div>
